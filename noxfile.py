eac00d794c05cef8a7386491ddeafe7d4e59b398
Mark Vartanyan <kolypto@gmail.com>
2020-10-24 12:37:04 +0300

    Python 3.9 tested
    works, but there's a nasty bug with pydantic

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ['tests']
 
 
-@nox.session(python=['3.8'])
+@nox.session(python=['3.8', '3.9'])
 def tests(session: nox.sessions.Session):
     """ Run all tests """
     session.install('poetry')
