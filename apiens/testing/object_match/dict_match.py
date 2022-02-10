0f3ccede4eb8386658db8981e13e98bab4f17358
Mark Vartanyan <kolypto@gmail.com>
2022-02-10 14:10:39 +0300

    testing.object_match.dict_match: improvements

--- apiens/testing/object_match/dict_match.py
+++ apiens/testing/object_match/dict_match.py
@@ -1,16 +1,30 @@
+from collections import abc
+
+
 class DictMatch(dict):
     """ Partial match of a dictionary to another one, with nice diffs
+
+    NOTE: it won't work with dict subclasses! Convert them to pure `dict` first :)
 
     Example:
         assert DictMatch({'praenomen': 'Gaius'}) == {'praenomen': 'Gaius', 'nomen': 'Julius', 'cognomen': 'Caesar'}
     """
     def __eq__(self, other):
         # Only equal if have the same type
-        if not isinstance(other, dict):
+        if not isinstance(other, abc.Mapping):
             return False
 
-        # Copy the missing values from another dict (in order to have nice diffs)
-        self.update({k: v for k, v in other.items() if k not in self})
+        # for key, value in self.items():
+        #     other_value = other[key]
+        #     values_are_equal = key in other and other_value == value
+        #     assert values_are_equal, f'{other!r} == {self!r} because of `{key}` {value!r} != {other_value}'
 
         # Compare
-        return all(other[name] == value for name, value in self.items())
+        equal = all(other[name] == value for name, value in self.items())
+
+        # Copy the missing values from another dict (in order to have nice diffs)
+        if not equal:
+            self.update({k: v for k, v in other.items() if k not in self})
+
+        # Done
+        return equal
