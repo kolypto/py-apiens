11e2e5111df81595a1f36d610d6df5461d349cb4
Mark Vartanyan <kolypto@gmail.com>
2022-03-02 17:03:56 +0300

    ariadne.directives sdl

--- apiens/tools/ariadne/directives/__init__.py
+++ apiens/tools/ariadne/directives/__init__.py
@@ -7,3 +7,8 @@ directives_map = {
     inherits.DIRECTIVE_NAME: inherits.InheritsDirective,
     partial.DIRECTIVE_NAME: partial.PartialDirective,
 }
+
+directives_sdl = '\n'.join((
+    inherits.DIRECTIVE_SDL,
+    partial.DIRECTIVE_SDL,
+))
