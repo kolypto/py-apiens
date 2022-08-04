cd4462deb3f26d57553f3a60350880fbc583784c
Mark Vartanyan <kolypto@gmail.com>
2022-08-04 12:10:09 +0300

    Cosmetic changes to: apiens/structure

--- apiens/structure/func/__init__.py
+++ apiens/structure/func/__init__.py
@@ -1,4 +1,1 @@
 """ Structured functions """
-from .decomarker import decomarker
-from .documented_errors import documented_errors, UndocumentedError, ErrorDoc
-from .simple_function import simple_function, LongFunctionError
