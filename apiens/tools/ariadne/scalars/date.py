c743844f05259d3a16d371831e4026ff7b4187f7
Mark Vartanyan <kolypto@gmail.com>
2022-08-04 12:27:13 +0300

    Refactoring: broken up scalars into graphql/scalars and ariadne/scalars
    Most of the code is now usable with other GraphQL frameworks.
    Ariadne bindings are optional.

--- apiens/tools/ariadne/scalars/date.py
+++ apiens/tools/ariadne/scalars/date.py
@@ -11,147 +11,37 @@ Usage:
     )
 """
 
-import pytz  # type: ignore[import]
-import datetime
-from typing import Any
 from ariadne import ScalarType
-
-from apiens.tools.translate import _
+from apiens.tools.graphql.scalars import date
 
 
 DateUTC = ScalarType('DateUTC')
-
-@DateUTC.serializer
-def serialize_date_utc(value: datetime.date) -> str:
-    return value.isoformat()
-
-@DateUTC.value_parser
-def parse_date_utc(value: Any) -> datetime.date:
-    try:
-        # Any thrown error is reported by GraphQL as "incorrect value". So we don't care about getting a boolean or an integer
-        return datetime.date.fromisoformat(value)
-
-        # NOTE: date.fromisoformat() does not support timezone offsets: i.e. "2022-01-01+02:00" is not valid.
-        # We do not have to remove it then.
-    except Exception as e:
-        raise ValueError(_("Not a valid date")) from e
-
+DateUTC.set_serializer(date.serialize_date_utc)
+DateUTC.set_value_parser(date.parse_date_utc)
 
 DateTimeUTC = ScalarType('DateTimeUTC')
-
-@DateTimeUTC.serializer
-def serialize_datetime_utc(value: datetime.datetime, *, convert: bool = True) -> str:
-    # Deal with the timezone.
-    # Default behavior: convert
-    if convert:
-        if value.tzinfo:
-            value = value.astimezone(pytz.UTC).replace(tzinfo=None)
-    else:
-        assert value.tzinfo in (None, datetime.timezone.utc, pytz.utc)
-
-    # Format
-    return value.replace(tzinfo=None).isoformat(' ', timespec='seconds') + 'Z'
-
-@DateTimeUTC.value_parser
-def parse_datetime_utc(value: Any) -> datetime.datetime:
-    try:
-        # RFC3339 supports "Z" offset. Our ISO parser does not.
-        value = value.removesuffix('Z')
-        value = datetime.datetime.fromisoformat(value)
-    except Exception as e:
-        raise ValueError(_("Not a valid date/time")) from e
-
-    # If the timezone was given -- convert to UTC and make it naive
-    if value.tzinfo is not None:
-        value = value.astimezone(pytz.UTC).replace(tzinfo=None)
-
-    # Done
-    return value
-
+DateTimeUTC.set_serializer(date.serialize_datetime_utc)
+DateTimeUTC.set_value_parser(date.parse_datetime_utc)
 
 LiteralDate = ScalarType('LiteralDate')
-LiteralDate.set_serializer(serialize_date_utc)
-LiteralDate.set_value_parser(parse_date_utc)
-
+LiteralDate.set_serializer(date.serialize_date_utc)
+LiteralDate.set_value_parser(date.parse_date_utc)
 
 LiteralTime = ScalarType('LiteralTime')
-
-@LiteralTime.serializer
-def serialize_literal_time(value: datetime.time) -> str:
-    assert value.tzinfo is None  # Literal time must be naive. Not even UTC: just naive.
-    return value.isoformat(timespec='seconds')
-
-@LiteralTime.value_parser
-def parse_literal_time(value: Any) -> datetime.time:
-    try:
-        value = datetime.time.fromisoformat(value)
-    except Exception as e:
-        raise ValueError(_("Not a valid time")) from e
-
-    # NOTE: time.fromisoformat() supports timezone offsets: "00:00+02:00" is supported.
-    # We need to make sure that the user cannot do this.
-    if value.tzinfo is not None:
-        raise ValueError(_("Literal time cannot have timezone associated with it"))
-
-    # Done
-    return value
-
+LiteralTime.set_serializer(date.serialize_literal_time)
+LiteralTime.set_value_parser(date.parse_literal_time)
 
 LiteralDateTime = ScalarType('LiteralDateTime')
-
-@LiteralDateTime.serializer
-def serialize_literal_datetime(value: datetime.datetime) -> str:
-    assert value.tzinfo is None  # literal datetime must be naive. Not even UTC: just naive.
-    return value.isoformat(' ', timespec='seconds')
-
-@LiteralDateTime.value_parser
-def parse_literal_datetime(value: Any) -> datetime.datetime:
-    try:
-        value = datetime.datetime.fromisoformat(value)
-    except Exception as e:
-        raise ValueError(_("Not a valid date/time")) from e
-
-    # Reject timezone
-    if value.tzinfo is not None:
-        raise ValueError(_("Timezones not allowed with literal date/times"))
-
-    # Done
-    return value
-
+LiteralDateTime.set_serializer(date.serialize_literal_datetime)
+LiteralDateTime.set_value_parser(date.parse_literal_datetime)
 
 DateTimeWithTimezone = ScalarType('DateTimeWithTimezone')
-
-@DateTimeWithTimezone.serializer
-def serialize_datetime_with_timezone(value: datetime.datetime) -> str:
-    assert value.tzinfo is not None  # must be aware datetime. No assumptions.
-    return value.isoformat(' ', timespec='seconds')
-
-@DateTimeWithTimezone.value_parser
-def parse_datetime_with_timezone(value: Any) -> datetime.datetime:
-    try:
-        value = datetime.datetime.fromisoformat(value)
-    except Exception as e:
-        raise ValueError(_("Not a valid date/time")) from e
-
-    # Require timezone
-    if value.tzinfo is None:
-        # No assumptions
-        raise ValueError(_("Timezone information must be included"))
-
-    # Done
-    return value
-
+DateTimeWithTimezone.set_serializer(date.serialize_datetime_with_timezone)
+DateTimeWithTimezone.set_value_parser(date.parse_datetime_with_timezone)
 
 TimezoneName = ScalarType('TimezoneName')
-
-@TimezoneName.value_parser
-def parse_timezone_name(value: Any) -> str:
-    try:
-        pytz.timezone(value)
-    except pytz.UnknownTimeZoneError as e:
-        raise ValueError(_("Invalid time zone name")) from e
-    else:
-        return value
+TimezoneName.set_serializer(date.serialize_timezone_name)
+TimezoneName.set_value_parser(date.parse_timezone_name)
 
 
 # Exported definitions
@@ -161,4 +51,6 @@ definitions = [
     LiteralDate,
     LiteralTime,
     LiteralDateTime,
+    DateTimeWithTimezone,
+    TimezoneName,
 ]
