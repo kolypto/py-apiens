3e2387b6d8bfde43b67de8f37a84f73a73f62b4a
Mark Vartanyan <kolypto@gmail.com>
2022-09-07 20:41:39 +0300

    shortcuts: import some tools from jessiql

---
+++ apiens/tools/sqlalchemy/testing/recreate_tables.py
@@ -0,0 +1,14 @@
+from jessiql.testing.recreate_tables import (
+    check_recreate_necessary,
+    recreate_db_tables,
+    truncate_db_tables,
+    truncate_or_recreate_db_tables,
+)
+from jessiql.testing.recreate_tables import (
+    create_tables,
+    drop_tables,
+    drop_existing_tables,
+)
+from jessiql.testing.recreate_tables import (
+    created_tables,
+)
